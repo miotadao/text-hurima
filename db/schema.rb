d49871c35f91e26d6a637dfda79b237ea1826716
mio <>
2024-11-11 00:01:32 +0900

    データベースをpostgreSQLに変更

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2024_08_16_141153) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -48,15 +51,15 @@ ActiveRecord::Schema[7.0].define(version: 2024_08_16_141153) do
     t.boolean "is_receive"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
+    t.bigint "user_id"
     t.integer "bought_id"
     t.string "author"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "talks", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "post_id", null: false
+    t.bigint "user_id", null: false
+    t.bigint "post_id", null: false
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
