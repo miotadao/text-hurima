d0f03297f84a56fae3ac40f1cad8bd641d41ab06
mio <>
2024-11-26 23:18:53 +0900

    production.rbとnorifier_mailer.rbとviewディレクトリ修正

--- app/mailers/notifier_mailer.rb
+++ app/mailers/notifier_mailer.rb
@@ -1,21 +1,24 @@
 class NotifierMailer < ApplicationMailer
-  default from: 'no-reply@example.com'
+  default from: 'no-reply@text-hurima.com'
 
   def item_sold_email(seller, post)
     @seller = seller
     @post = post
+    @url  = 'https://text-hurima.com/login'
     mail(to: @seller.email, subject: 'あなたの商品が購入されました。')
   end
 
   def item_cancel_email(seller, post)
     @seller = seller
     @post = post
+    @url  = 'https://text-hurima.com/login'
     mail(to: @seller.email, subject: 'あなたの商品がキャンセルされました。')
   end
 
   def talk_email(receiver, post)
     @receiver = receiver
     @post = post
+    @url  = 'https://text-hurima.com/login'
     mail(to: @receiver.email, subject: '相手からチャットのコメントが来ました。')
   end
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,16 +91,16 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-    
+    config.action_mailer.default_url_options = { host: 'text-hurima-70a859267649.herokuapp.com' }
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.perform_deliveries = true
     config.action_mailer.smtp_settings = {
       address: 'smtp.sendgrid.net',
+      port: 587,
+      domain: 'herokuapp.com',
       user_name: 'apikey',
       password: ENV['SENDGRID_API_KEY'],
-      authentication: 'plain',
-      domain: 'herokuapp.com',
-      port: 587,
+      authentication: :plain,
       enable_starttls_auto: true
   }
 
