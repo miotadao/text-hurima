a83abb9d8c3323bc7ade067336d9f5bd3239d4aa
mio <>
2024-11-26 23:50:20 +0900

    production.rb修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,7 +99,7 @@ Rails.application.configure do
       port: 587,
       domain: 'herokuapp.com',
       user_name: 'apikey',
-      password: ENV['SENDGRID_API_KEY'],
+      password: ENV['SENDGRID_APIKEY'],
       authentication: :plain,
       enable_starttls_auto: true
   }
