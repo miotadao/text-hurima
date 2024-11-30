56944c84bcb3ffe705bd0f4ef52fa6bc38bbcd61
mio <>
2024-11-30 14:27:17 +0900

    enviroment.rb修正

--- config/environment.rb
+++ config/environment.rb
@@ -4,12 +4,3 @@ require_relative "application"
 # Initialize the Rails application.
 Rails.application.initialize!
 
-ActionMailer::Base.smtp_settings = {
-  user_name: 'apikey',
-  password: Rails.application.credentials.dig(:sendgrid, :api_key),
-  domain: 'text-hurima-70a859267649.herokuapp.com',
-  address: 'smtp.sendgrid.net',
-  port: 587,
-  authentication: :plain,
-  enable_starttls_auto: true
-}
